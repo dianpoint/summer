6732fa248573caa21dfd8ad40f0e85e37edcc725
ccoderJava <congccoder@gmail.com>
2025-05-20 21:04:07 +0800

    :sparkles: 添加注解校验处理器

---
+++ src/main/java/com/dianpoint/summer/validation/processor/AnnotationProcessorRegister.java
@@ -0,0 +1,10 @@
+package com.dianpoint.summer.validation.processor;
+
+/**
+ * @author: congccoder
+ * @email: congccoder@gmail.com | <a href="https://github.com/ccoderJava">github-homepage</a>
+ * @date: 2025/5/20 20:59
+ */
+
+public class AnnotationProcessorRegister {
+}
