386ac51406838fb90f36f684ee865af815e2fb41
ccoderJava <congccoder@gmail.com>
2025-06-05 20:45:42 +0800

    :sparkles: add functions

---
+++ summer-java-core/src/main/java/com/dianpoint/summer/lang/Streams.java
@@ -0,0 +1,10 @@
+package com.dianpoint.summer.lang;
+
+/**
+ * @author: congccoder
+ * @email: congccoder@gmail.com | <a href="https://github.com/ccoderJava">github-homepage</a>
+ * @date: 2025/6/5 20:43
+ */
+
+public interface Streams {
+}
