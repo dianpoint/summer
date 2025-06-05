d559e35c6bbb84678e0f4a021ed2a06fd6ba6705
ccoderJava <congccoder@gmail.com>
2025-06-05 20:48:29 +0800

    :sparkles: add  throwable functions

--- summer-java-core/src/main/java/com/dianpoint/summer/lang/ThrowableSupplier.java
+++ summer-java-core/src/main/java/com/dianpoint/summer/lang/ThrowableSupplier.java
@@ -1,5 +1,9 @@
 package com.dianpoint.summer.lang;
 
+import java.util.function.Function;
+
+import static com.dianpoint.summer.utils.AssertUtils.assertNotNull;
+
 /**
  * @author: congccoder
  * @email: congccoder@gmail.com | <a href="https://github.com/ccoderJava">github-homepage</a>
@@ -10,4 +14,34 @@ package com.dianpoint.summer.lang;
 public interface ThrowableSupplier<T> {
 
     T get() throws Throwable;
+
+    default T execute() {
+        return execute(this::handleException);
+    }
+
+    default T handleException(Throwable failure) {
+        throw new RuntimeException(failure);
+    }
+
+    default T execute(Function<Throwable, T> exceptionHandler) {
+        assertNotNull(exceptionHandler, () -> "The 'exceptionHandler' must not be null");
+        T result;
+        try {
+            result = get();
+        } catch (Throwable e) {
+            result = exceptionHandler.apply(e);
+        }
+        return result;
+    }
+
+    static <T> T execute(ThrowableSupplier<T> supplier) throws NullPointerException {
+        return execute(supplier, supplier::handleException);
+    }
+
+
+    static <T> T execute(ThrowableSupplier<T> supplier, Function<Throwable, T> exceptionHandler) throws NullPointerException {
+        assertNotNull(supplier, "The supplier must not be null");
+        return supplier.execute(exceptionHandler);
+    }
+
 }
---
+++ summer-java-core/src/main/java/com/dianpoint/summer/utils/Utils.java
@@ -0,0 +1,10 @@
+package com.dianpoint.summer.utils;
+
+/**
+ * @author: congccoder
+ * @email: congccoder@gmail.com | <a href="https://github.com/ccoderJava">github-homepage</a>
+ * @date: 2025/6/5 20:46
+ */
+
+public interface Utils {
+}
