c05668325388c78c9b7000620e1df3ff1ea9b1b4
ccoderJava <congccoder@gmail.com>
2025-06-27 16:33:09 +0800

    add unitTest

---
+++ summer-java-core/src/test/java/com/dianpoint/summer/lang/PredicatesTest.java
@@ -0,0 +1,10 @@
+package com.dianpoint.summer.lang;
+
+/**
+ * @author: congccoder
+ * @email: congccoder@gmail.com | <a href="https://github.com/ccoderJava">github-homepage</a>
+ * @date: 2025/6/27 16:32
+ */
+
+public class PredicatesTest {
+}
